ab15bf2f7114a694f0c106d6b80f305ca60fc44f
leus8 <leoagv88@gmail.com>
2021-05-31 02:42:42 -0600

    area casi listo, falta mejorar iterador de nounce

--- pkg/hash/hash_area.go
+++ pkg/hash/hash_area.go
@@ -26,57 +26,81 @@ func (ha *HashArea) Next(nonce *Nonce) *Nonce {
 }
 
 func (ha *HashArea) MicroHashUcr(p Payload, n Nonce) Bounty {
-	b := Bloque{}
+	bloc := Bloque{}
 	w := make([]byte, 32)
 
 	// Concatenacion del nonce y el array de bytes de entrada
-	copy(b[:], p[:])       // de la posicion 0 - 12 los bytes de entrada
-	copy(b[len(p):], n[:]) // de la posicion 12-16 el nonce
+	copy(bloc[:], p[:])       // de la posicion 0 - 12 los bytes de entrada
+	copy(bloc[len(p):], n[:]) // de la posicion 12-16 el nonce
 
 	for i := 0; i <= 15; i++ {
-		w[i] = b[i]
+		w[i] = bloc[i]
 	}
 	for i := 16; i <= 31; i++ {
-		w[i] = w[i-3] | w[i-9] ^ w[i-14]
+		w[i] = w[i-3] | (w[i-9] ^ w[i-14])
 	}
 
 	h := [3]byte{0x01, 0x89, 0xfe}
 
 	var k byte
 	var x byte
+
+	var a byte = h[0]
+	var b byte = h[1]
+	var c byte = h[2]
+
 	for i := 0; i <= 31; i++ {
 
-		var a byte = h[0]
-		var b byte = h[1]
-		var c byte = h[2]
+		// var a byte = h[0]
+		// var b byte = h[1]
+		// var c byte = h[2]
 
 		if i <= 0 && i <= 16 {
 			k = 0x99
-			x = a ^ b
+			x = (a ^ b) & 0xFF
 		} else if i <= 17 && i <= 31 {
 			k = 0xa1
-			x = a | b
+			x = (a | b) & 0xFF
 		}
 
-		a = b ^ c
-		b = c << 4
-		c = x + k + w[i]
+		a = (b ^ c) & 0xFF
+		b = (c << 4) & 0xFF
+		c = (x + k + w[i]) & 0xFF
 
-		if i == 31 {
-			h[0] = h[0] + a
-			h[1] = h[1] + b
-			h[2] = h[2] + c
-		}
+		// if i == 31 {
+		// 	h[0] = h[0] + a
+		// 	h[1] = h[1] + b
+		// 	h[2] = h[2] + c
+		// }
 
 	}
+
+	h[0] = (h[0] + a) & 0xFF
+	h[1] = (h[1] + b) & 0xFF
+	h[2] = (h[2] + c) & 0xFF
 
 	// Casteo a tipo de bounty
 	return Bounty(h)
 
 }
 
-func (h *HashArea) ValidateBounty(target byte) {
+func (h *HashArea) ValidateBounty(target byte, bounty Bounty) int {
 
 	// Esta funcion valida si el bounty calculado esta dentro del target especificado
+
+	var valid int  // define el valido
+	var valido int // retorno
+	valid = 0
+
+	if bounty[0] < target && bounty[1] < target {
+		valid = 1
+
+	} else {
+		valid = 0
+	}
+
+	valido = valid
+
+	return valido
 
 }
--- pkg/hash/hash_test.go
+++ pkg/hash/hash_test.go
@@ -8,14 +8,33 @@ import (
 )
 
 func TestHashArea(t *testing.T) {
-	testPayload := hash.Payload{0x39, 0x7d, 0x9f, 0x2f, 0x40, 0xca, 0x9e, 0x6c, 0x6b, 0x1f, 0x33, 0x24}
-	testNonce := hash.Nonce{0xfd, 0xed, 0x87, 0x3c}
+	testPayload := hash.Payload{0x39, 0x7d, 0x9f, 0x2f, 0x40, 0xca, 0x9e, 0x6c, 0x6b, 0x1f, 0x33, 0x24} // datos de entrada
+	testNonce := hash.Nonce{0x00, 0x00, 0x00, 0x00}                                                     // nonce de entrada, se inicializa en 0,0,0,0
 
+	var bounty hash.Bounty
+	terminado := 0
 	ha := hash.HashArea{}
-	bounty := ha.MicroHashUcr(testPayload, testNonce)
 
-	fmt.Println("El bounty es:", bounty)
+	i := 0
 
-	nextNonce := ha.Next(&testNonce)
-	fmt.Println("El nuevo nonce es:", *nextNonce)
+	for (terminado == 0) && (i <= 1024) {
+
+		fmt.Printf("Entrada: [%# x]\n", testPayload[:]) // imprime el bounty obtenido
+		fmt.Printf("Nounce: [%# x]\n", testNonce[:])    // imprime el nounce utilizado
+		fmt.Printf("\n")                                // nueva linea
+
+		bounty = ha.MicroHashUcr(testPayload, testNonce) // obtiene el bounty
+		terminado = ha.ValidateBounty(10, bounty)        // verifica que sea un bounty correcto
+
+		if terminado == 0 {
+			ha.Next(&testNonce) // si no se ha terminado, calcular proximo nounce
+		}
+
+		i = i + 1
+
+	}
+
+	fmt.Printf("El proceso ha terminado.\n")
+	fmt.Printf("El bounty obtenido es: [%# x]\n", bounty[:]) // imprime el bounty obtenido
+
 }
